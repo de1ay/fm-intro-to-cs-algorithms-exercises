112d4dab0d5debbfcea0df9ceb7008e861b196cc
Nikita Ivanov <github@de1ay.me>
2024-11-16 23:20:47 +0300

    +: insertion sort

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -13,13 +13,23 @@
   And you put xdescribe instead of describe if you want to suspend running the unit tests.  
 */
 
+/**
+ * @param {Array<Number>} nums 
+ */
 function insertionSort(nums) {
-  // code goes here
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (nums[j] >= nums[j-1]) break;
+      const tmp = nums[j];
+      nums[j] = nums[j-1];
+      nums[j-1] = tmp;
+    }
+  }
 }
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
